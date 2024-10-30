f3455342fb190cb740b36ad1688d43b48a1317dd
Abdykali Azat <azat.abdykalli@gmail.com>
2024-10-31 02:29:37 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+    apiUrl: 'http://localhost:3001',
+    // apiUrl: 'https://cargoasem17.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'CARGO ASEM17'
+  };
+  
+  
+export default config;
+  
